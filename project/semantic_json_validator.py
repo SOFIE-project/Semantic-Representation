480601f0b3396a3860162529d9119b2f5785ceb9
Filippo Vimini <filippo.vimini@ericsson.com>
2020-02-25 10:43:36 +0200

    Test validation-schema

--- project/semantic_json_validator.py
+++ project/semantic_json_validator.py
@@ -4,6 +4,7 @@ import os
 
 from project import app
 
+
 def validate_semantic(json_to_validate):
     validation_results = {'validation': True, 'error': None}
     try:
@@ -14,13 +15,10 @@ def validate_semantic(json_to_validate):
         validation_results['error'] = 'schema_not_found'
     except (jsonschema.exceptions.SchemaError, jsonschema.exceptions.ValidationError) as validation_error:
         validation_results['validation'] = False
-        validation_results['error'] = validation_error 
+        validation_results['error'] = validation_error
     return validation_results
 
+
 def validate_schema(json_to_validate, schema):
-    try:
-        print('JSON to validate: \n', json_to_validate)
-        print('TD schema: \n', schema)
-        jsonschema.validate(instance=json_to_validate, schema=schema)
-    except (jsonschema.exceptions.SchemaError, jsonschema.exceptions.ValidationError):
-        raise
+    jsonschema.validate(instance=json_to_validate, schema=schema)
+
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,24 +1,36 @@
 import jsonschema
 import json
 import sys
+import unittest
 
 sys.path.append(".")  # Adds higher directory to python modules path.
 
 from project import semantic_json_validator
 
 
-def test_json_validation_schema():
-    iot_schema = json.loads(open('project/static/W3C_IoT_ThingDescription_schema.json').read())
-    smaug_schema = json.loads(open('project/static/smaug_test_sofie_schema.json').read())
-    # rovio_schema = json.loads(open('project/static/rovio_test_sofie_schema.json').read())
-    # smaug_obj = json.loads(open('project/static/smaug_locker_td.json').read())
-    try:
-        semantic_json_validator.validate_schema(smaug_schema, iot_schema)
-        # semantic_json_validator.validate_schema(smaug_schema, iot_schema)
-        # semantic_json_validator.validate_schema(rovio_schema, iot_schema)
-    except jsonschema.exceptions.ValidationError as error:
-        print(error)
+class ValidatorTestClass(unittest.TestCase):
+
+    def test_json_validation_schema(self):
+        iot_schema = json.loads(open('project/static/W3C_IoT_ThingDescription_schema.json').read())
+        smaug_schema = json.loads(open('project/static/smaug_test_sofie_schema.json').read())
+        smaug_obj_invalid = json.loads(open('project/static/smaug_locker_invalid_obj.json').read())
+        smaug_obj_invalid_price_low = json.loads(open('project/static/smaug_locker_invalid_price_low.json').read())
+        smaug_obj_invalid_price_high = json.loads(open('project/static/smaug_locker_invalid_price_high.json').read())
+        smaug_obj_valid = json.loads(open('project/static/smaug_locker_valid_obj.json').read())
+        smaug_obj_invalid_volume = json.loads(open('project/static/smaug_locker_invalid_volume.json').read())
+
+        self.assertIsNone(semantic_json_validator.validate_schema(smaug_schema, iot_schema))
+        self.assertIsNone(semantic_json_validator.validate_schema(smaug_obj_valid, smaug_schema))
+        self.assertRaises(jsonschema.exceptions.ValidationError, semantic_json_validator.validate_schema,
+                          smaug_obj_invalid, smaug_schema)
+        self.assertRaises(jsonschema.exceptions.ValidationError, semantic_json_validator.validate_schema,
+                          smaug_obj_invalid_price_low, smaug_schema)
+        self.assertRaises(jsonschema.exceptions.ValidationError, semantic_json_validator.validate_schema,
+                          smaug_obj_invalid_price_high, smaug_schema)
+        self.assertRaises(jsonschema.exceptions.ValidationError, semantic_json_validator.validate_schema,
+                          smaug_obj_invalid_volume, smaug_schema)
 
 
+# runs the unit tests in the module
 if __name__ == '__main__':
-    test_json_validation_schema()
+    unittest.main()
