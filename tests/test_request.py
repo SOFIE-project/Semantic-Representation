c6e74a714eef4dbb56a2c30d7bfad9be77be109e
Filippo Vimini <filippo.vimini@ericsson.com>
2020-03-03 15:15:26 +0200

    clean tests

--- tests/test_request.py
+++ tests/test_request.py
@@ -2,31 +2,34 @@ import requests
 import yaml
 import unittest
 import json
+import sys
+
+sys.path.append(".")  # Adds higher directory to python modules path.
+
+from project import app
 
 
 class FunctionalTestClass(unittest.TestCase):
 
     def test_valid_requests(self):
-        yaml_config = yaml.load(open('tests/static/config.yaml'), Loader=yaml.FullLoader)
         try:
             valid_jsons = json.loads(open('tests/static/custom_valid_requests.json').read())
         except FileNotFoundError:
             valid_jsons = json.loads(open('tests/static/default_valid_requests.json').read())
 
-        url = 'http://'+str(yaml_config['host'])+':'+str(yaml_config['port'])+'/api/v1/validate'
+        url = 'http://'+str(app.config['HOST'])+':'+str(app.config['PORT'])+'/api/v1/validate'
 
         for x in valid_jsons:
             res = json.loads(requests.post(url, json=x, headers={}).text)
             self.assertEqual("success", res['status'])
 
     def test_invalid_requests(self):
-        yaml_config = yaml.load(open('tests/static/config.yaml'), Loader=yaml.FullLoader)
         try:
             invalid_jsons = json.loads(open('tests/static/custom_invalid_requests.json').read())
         except FileNotFoundError:
             invalid_jsons = json.loads(open('tests/static/default_invalid_requests.json').read())
 
-        url = 'http://'+str(yaml_config['host'])+':'+str(yaml_config['port'])+'/api/v1/validate'
+        url = 'http://'+str(app.config['HOST'])+':'+str(app.config['PORT'])+'/api/v1/validate'
 
         for x in invalid_jsons:
             res = json.loads(requests.post(url, json=x, headers={}).text)
